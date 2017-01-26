d0c69f340779c39a39398d1d4de80b7d3368d9ed
Jeff Crowe <jeff@jeffjcrowe.com>
2017-01-25 17:59:58 -0800

    dockerhub

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker tag youtube-live-stream idlerun/youtube-live-stream
-docker tag youtube-live-cleanup idlerun/youtube-live-cleanup
-#docker tag youtube-live-download idlerun/youtube-live-download
-
-docker push idlerun/youtube-live-stream
-docker push idlerun/youtube-live-cleanup
-#docker push idlerun/youtube-live-download
