9ebb91b0fc1f5c3764c9a55d020810f77acf7642
Jeff Crowe <jeff@jeffjcrowe.com>
2017-01-31 16:01:11 -0800

    fix missing xargs

--- cleanup/entrypoint.sh
+++ cleanup/entrypoint.sh
@@ -9,7 +9,7 @@ cd /data
 while :
 do
   echo "Cleaning up old recordings"
-  ls -1 /data/*-*-*_*-*-*.mp4 | head -n -48 | rm -vf
+  ls -1 /data/*-*-*_*-*-*.mp4 | head -n -48 | xargs rm -vf
   echo "Waiting for 1 hour"
   sleep 3600
 done
